9ee564ea76f7fab13bfc39a42c54e4b331bd109f
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2012-11-04 18:25:25 -0200

    Eclipse formatting.

--- src/main/java/br/usp/ime/tla/LogProcessor.java
+++ src/main/java/br/usp/ime/tla/LogProcessor.java
@@ -16,9 +16,9 @@ import java.util.Set;
 public class LogProcessor {
 
 	private final String applicationLogPattern = "^# (orch|chor),(\\d+),(\\d+),(\\d+)";
-	private final String tomcatPattern = "" +
-			"^(\\d{1,3}\\.){3}\\d{1,3} - - \\[(\\d){2}/(\\w){3}/(\\d){4}(:\\d{2}){3} -\\d{4}\\] " +
-			"\"POST (.+) (HTTP/1\\.1)\"( \\d+){3}";
+	private final String tomcatPattern = ""
+			+ "^(\\d{1,3}\\.){3}\\d{1,3} - - \\[(\\d){2}/(\\w){3}/(\\d){4}(:\\d{2}){3} -\\d{4}\\] "
+			+ "\"POST (.+) (HTTP/1\\.1)\"( \\d+){3}";
 
 	HashMap<Long, String> entries;
 	private ArrayList<Long> keys;
@@ -44,7 +44,7 @@ public class LogProcessor {
 	 */
 	private void populatesLogMap() {
 		entries = new HashMap<Long, String>();
-		
+
 		try {
 			this.getExperimentEntryMap();
 		} catch (FileNotFoundException e) {
@@ -53,19 +53,23 @@ public class LogProcessor {
 	}
 
 	/*
-	 * Reads the file entered as the first argument of TomcatLogAnalyzer, 
-	 * and parses it to ge a Map of entries
+	 * Reads the file entered as the first argument of TomcatLogAnalyzer, and
+	 * parses it to ge a Map of entries
 	 */
 	public void getExperimentEntryMap() throws FileNotFoundException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				new FileInputStream(fileName)));
 		String line = null;
 		try {
 			while ((line = reader.readLine()) != null) {
-				if(this.matchesApplicationLog(line)) {
-					
+				if (this.matchesApplicationLog(line)) {
+
 					String exp = line.split(" ")[1];
-					entries.put(Long.parseLong(exp.split(",")[3]), 
-							exp.split(",")[0].concat(","+exp.split(",")[1].concat(","+exp.split(",")[2])));
+					entries.put(
+							Long.parseLong(exp.split(",")[3]),
+							exp.split(",")[0].concat(","
+									+ exp.split(",")[1].concat(","
+											+ exp.split(",")[2])));
 
 				}
 			}
@@ -73,9 +77,10 @@ public class LogProcessor {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/*
 	 * @param string an entry of experiment log
+	 * 
 	 * @return true if string matches # orch|chor,n,n,long
 	 */
 	public boolean matchesApplicationLog(String line) {
@@ -86,24 +91,26 @@ public class LogProcessor {
 	 * Returns a string representation of an instance of experiment
 	 * 
 	 * @param l a long representing a date in millis
+	 * 
 	 * @return a string like orch,1,100
 	 */
 	public String getParams(long l) {
-		if(keys.contains(l)) 
+		if (keys.contains(l))
 			return entries.get(l);
 
 		int index = 0;
 		while (keys.get(index) < l && index < keys.size())
 			index++;
 
-		if(index >= keys.size()) 
-			return entries.get(keys.get(keys.size()-1));
+		if (index >= keys.size())
+			return entries.get(keys.get(keys.size() - 1));
 
-		return entries.get(keys.get(index-1));
+		return entries.get(keys.get(index - 1));
 	}
 
 	/*
 	 * @param date a string representation of a date e.g. 16/Oct/2012:00:00:08
+	 * 
 	 * @return Date object if date can be converted to Date and null otherwise
 	 */
 	public Date getDateInMillis(String date) {
@@ -119,19 +126,20 @@ public class LogProcessor {
 
 	/*
 	 * @param string an entry of apache tomcat log
+	 * 
 	 * @return true if string matches tomcatPattern
 	 */
 	public boolean matchesTomcatPattern(String string) {
 		return string.matches(tomcatPattern);
 	}
-	
+
 	/*
 	 * Used only to test
 	 */
 	public HashMap<Long, String> getExperimentEntries() {
 		return entries;
 	}
-	
+
 	/*
 	 * Used only to test
 	 */
--- src/test/java/br/usp/ime/tla/LogProcessorTest.java
+++ src/test/java/br/usp/ime/tla/LogProcessorTest.java
@@ -60,18 +60,18 @@ public class LogProcessorTest {
 
 	@Test
 	public void testLogTomcatMatching() {
-		assertTrue(entries.matchesTomcatPattern(
-				"198.55.32.149 - - [16/Oct/2012:00:00:08 -0700] \"POST /registry/endpoint HTTP/1.1\" 200 590 1"));
-		assertTrue(entries.matchesTomcatPattern(
-				"198.55.32.149 - - [10/Oct/2012:05:26:28 -0700] \"POST /supermarket3/orchestration HTTP/1.1\" 200 2264 4"));
-		assertFalse(entries.matchesTomcatPattern(
-				"178.237.128.23 - tomcat [10/Oct/2012:04:12:12 -0700] \"HEAD /manager/html HTTP/1.0\" 200 - 26"));
-		assertFalse(entries.matchesTomcatPattern(
-				"198.55.32.149 - - [10/Oct/2012:05:26:26 -0700] \"GET /supermarket3/orchestration?wsdl HTTP/1.1\" 200 2171 41"));
-		assertFalse(entries.matchesTomcatPattern(
-				"178.237.128.23 - - [10/Oct/2012:04:12:12 -0700] \"HEAD /manager/html HTTP/1.0\" 401 - 1"));
+		assertTrue(entries
+				.matchesTomcatPattern("198.55.32.149 - - [16/Oct/2012:00:00:08 -0700] \"POST /registry/endpoint HTTP/1.1\" 200 590 1"));
+		assertTrue(entries
+				.matchesTomcatPattern("198.55.32.149 - - [10/Oct/2012:05:26:28 -0700] \"POST /supermarket3/orchestration HTTP/1.1\" 200 2264 4"));
+		assertFalse(entries
+				.matchesTomcatPattern("178.237.128.23 - tomcat [10/Oct/2012:04:12:12 -0700] \"HEAD /manager/html HTTP/1.0\" 200 - 26"));
+		assertFalse(entries
+				.matchesTomcatPattern("198.55.32.149 - - [10/Oct/2012:05:26:26 -0700] \"GET /supermarket3/orchestration?wsdl HTTP/1.1\" 200 2171 41"));
+		assertFalse(entries
+				.matchesTomcatPattern("178.237.128.23 - - [10/Oct/2012:04:12:12 -0700] \"HEAD /manager/html HTTP/1.0\" 401 - 1"));
 	}
-	
+
 	@Test
 	public void testLogApplicationMatching() {
 		assertTrue(entries.matchesApplicationLog("# orch,2,50,1349926584487"));
